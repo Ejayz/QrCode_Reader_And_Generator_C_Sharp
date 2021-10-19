4526d5f9568ad08343f5463ddfba26cfdc6579fe
Aspect <32452006+aspectdev-ph@users.noreply.github.com>
2021-10-19 19:08:10 +0800

    This is the initial upload of files

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FaceRecognitionAttendanceSystem.Properties {
+namespace QrCode_Reader_And_Generator_C_Sharp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FaceRecognitionAttendanceSystem.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FaceRecognitionAttendanceSystem.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("QrCode_Reader_And_Generator_C_Sharp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
